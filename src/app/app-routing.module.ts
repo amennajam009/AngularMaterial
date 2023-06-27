6fd1bc831cd184f75567ca4137fd641b45b03155
amennajamm009 <amennajam7@gmail.com>
2023-06-27 13:53:03 +0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './MainModule/home/home/home.component';
 
 
 const routes: Routes = [
+  {path:'' , component:HomeComponent},
 {path:'home' , component:HomeComponent}
 
 ];
